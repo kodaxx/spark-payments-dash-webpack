d3f8c9f88cc52f00332e14cbf43513c9f47d05c6
Spencer Kuzara <kodaxx@gmail.com>
2019-05-21 12:13:11 -0600

    added dashtext logo

--- src/views/Qr.vue
+++ src/views/Qr.vue
@@ -12,7 +12,7 @@
       <qrcode v-show="qr" @click.native="test()" :value="uri" :options="{ size: 256, backgroundAlpha: 0, foregroundAlpha: 0.8, level: 'H', padding: 15 }" :tag="'img'"></qrcode>
       <div v-show="!qr" id="cointext" @click="test()">
         <div id="content">
-          <img id="cointext-logo" src='../assets/img/cointext.png'>
+          <img id="cointext-logo" src='../assets/img/dashtext.png'>
           <p id="invoice">{{ this.invoice }}</p>
         </div>
       </div>
